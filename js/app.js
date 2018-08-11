23de39c177518ef22e142ddbc8785950239a3bb7
haffs0 <abuafeezabolarinwa@yahoo.com>
2018-08-11 14:29:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ function cardMatch(value) {
     return value.reduce((n,m) => { n === m; });
 }
 
-function newCard() {
+function newGenerateCard() {
     cardMoves = 0;
     let output = ' ';
     cardFiles.shuffle();
@@ -27,22 +27,7 @@ function newCard() {
     }
     cardList.appendChild(output);
 }
-function cardFlipTile(cardd, val ) {
-    if (cardd.innerHTML === "" && cardValues.length < 2) {
-        cardd.classList.toggle("open");
-        cardd.classList.toggle("show");
-        let insideItem = '<i class=`${val}></i>';
-        cardd.appendChild(insideItem);
-        if (cardValues.length === 0) {
-            cardValues.push(val);
-            cardIds.push(cardd.id);
-        }
-        else if (cardValues === 1) {
-            cardValues.push(val);
-            cardIds.push(cardd.id);
-        }
-    }
-}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -69,3 +54,52 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+function cardFlipTile(cardd, val ) {
+    if (cardd.innerHTML === "" && cardValues.length < 2) {
+        cardd.classList.toggle("open");
+        cardd.classList.toggle("show");
+        let insideItem = '<i class=`${val}></i>';
+        cardd.appendChild(insideItem);
+        if (cardValues.length === 0) {
+            cardValues.push(val);
+            cardIds.push(cardd.id);
+        }
+        else if (cardValues === 1) {
+            cardValues.push(val);
+            cardIds.push(cardd.id);
+            if(cardMatch(...cardValues)) {
+                cardMoves += 2;
+                //Clear both arrays
+                cardValues = [];
+                cardIds = [];
+                //Checkedn to see if the while board is cleared
+                if(cardMoves === cardFiles.length) {
+                    alert("Congratulation! you won with `${cardMoves}` and Stars Wooooooo!");
+                    cardList.innerHTML = " ";
+                    newGenerateCard();
+                }
+            }
+        }
+    }
+    else {
+        function flipCard() {
+            if(!(cardMatch(...cardValues))) {
+                card1.classList.toggle('red');
+                card1.innerHTML = " ";
+                card2.classList.toggle('red');
+                card2.innerHTML = " ";
+                //Clear both arrays
+                cardValues = [];
+                cardIds = [];
+            }
+        }
+        setTimeOut(flipCard, 2000);
+    }
+}
+
+cardItem.addEventListener(click, function() {
+    cardFlipTile(this, cardFiles[shuffles()] );
+});
+
+
+
