ce0307dc690520026e977168ed2fe91488c963ff
haffs0 <abuafeezabolarinwa@yahoo.com>
2018-08-11 17:53:52 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-function cardFlipTile(cardd, val ) {
+/*function cardFlipTile(cardd, val ) {
     if (cardd.innerHTML === "" && cardValues.length < 2) {
         cardd.classList.toggle("open");
         cardd.classList.toggle("show");
@@ -98,7 +98,7 @@ function cardFlipTile(cardd, val ) {
 
 cardItem.addEventListener(click, function() {
     cardFlipTile(this, cardFiles[shuffles()] );
-});
+});*/
 
 newGenerateCard();
 
